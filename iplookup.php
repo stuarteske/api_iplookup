bc68b1ca884484c70ae59547b0ed61243d2b88ba
Stuart Eske <sdeske@googlemail.com>
2013-11-12 14:34:30 +0000

    v1.00.00

---
+++ iplookup.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stuarteske
+ * Date: 10/11/2013
+ * Time: 18:33
+ *
+ * @copyright    Copyright (C) 2013 CFA Group. All rights reserved.
+ * @license      GNU General Public License Version 2 or later.
+ * @author       Stuart Eske, <stuart.eske@cfa-group.com>
+ */
+
+header("Access-Control-Allow-Origin: *");
+
+$data = $_GET['domains'];
+
+$returnArray = array();
+
+foreach ($data as $key => $domain) {
+    $returnArray[$domain]['ip'] = gethostbyname($domain);
+    //$returnArray[$domain]['latency'] = pingDomain($domain);
+}
+
+
+echo json_encode($returnArray);
